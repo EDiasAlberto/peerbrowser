a0d084ecb8590815bfe5e2187d5b23dbbcb8907d
Ethan Dias Alberto <proxamon1@gmail.com>
2025-10-24 20:56:22 +0100

    feat: track parent directory of original file

--- browser-client/site_dependency.py
+++ browser-client/site_dependency.py
@@ -24,18 +24,19 @@ class SiteDependencies:
         trimmed_path = path_and_trailing_chars.split(")")[0]
         return trimmed_path.strip("'").strip('"')
 
-    def get_all_css_dependencies(self, filename: str) -> list[str]:
+    def get_all_css_dependencies(self, filename: str, parent_folder: str) -> list[str]:
         link_or_import_statements = []
         with open(filename) as fp:
             line = fp.readline()
             while line != "":
                 if self.css_is_import(line) or self.css_is_linked_statement(line):
                     import_path = self.css_get_import_path(line)
-                    link_or_import_statements.append(import_path)
+                    full_import_path = os.path.join(parent_folder, full_import_path)
+                    link_or_import_statements.append(full_import_path)
                 line = fp.readline()
         return link_or_import_statements
 
-    def get_all_html_dependencies(self, filename: str) -> list[str]:
+    def get_all_html_dependencies(self, filename: str, parent_folder: str) -> list[str]:
         with open(filename) as fp:
             soup = BeautifulSoup(fp, "html.parser")
         code_dependencies = soup.find_all(["link", "script"])
@@ -43,18 +44,25 @@ class SiteDependencies:
         for tag in code_dependencies: 
             if tag.name == "link":
                 if not self.html_is_inline_or_remote_ref(tag, "href"):
-                    filtered_dependencies.append(tag["href"])
+                    full_import_path = os.path.join(parent_folder, tag["href"])
             elif tag.name == "script":
                 if not self.html_is_inline_or_remote_ref(tag, "src"):
-                    filtered_dependencies.append(tag["src"])
+                    full_import_path = os.path.join(parent_folder, tag["src"])
+            if full_import_path:
+                filtered_dependencies.append(full_import_path)
         return filtered_dependencies
     
-    def get_all_js_dependencies(self, filename: str) -> list[str]:
+    def get_all_js_dependencies(self, filename: str, parent_folder: str) -> list[str]:
         # TODO: implement js parsing to find imports
         print(f"Checking {filename} for import statements")
 
+    def tidy_dependency_paths(self, parent_folder: str, dependencies: list[str]) -> list[str]:
+        tidied_dependencies = []
+        for dep in dependencies:
+            tidied_dependencies.append(os.path.join(parent_folder, dep))
+        return tidied_dependencies
 
 if __name__=="__main__":
     parser = SiteDependencies()
-    print(parser.get_all_html_dependencies("test-site/index.html"))
-    print(parser.get_all_css_dependencies("test-site/css/main.css"))
+    print(parser.get_all_html_dependencies("test-site/index.html", "test-site/"))
+    print(parser.get_all_css_dependencies("test-site/css/main.css", "test-site/"))
